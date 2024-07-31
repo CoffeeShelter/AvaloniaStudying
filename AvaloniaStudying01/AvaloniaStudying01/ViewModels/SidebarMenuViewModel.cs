eec196dce9bea91655b28ce178cf1d3a155b7bb6
CoffeeShelter <ij238950@gmail.com>
2024-07-31 22:10:05 +0900

    study sidebar, need to add code for viewmodel

---
+++ AvaloniaStudying01/AvaloniaStudying01/ViewModels/SidebarMenuViewModel.cs
@@ -0,0 +1,52 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using ReactiveUI;
+using System;
+using System.Collections.ObjectModel;
+
+namespace AvaloniaStudying01.ViewModels
+{
+    public partial class SidebarMenuViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private bool _isPaneOpen = true;
+
+        [ObservableProperty]
+        private ObservableObject _currentPage = new HomePageViewModel();
+
+        [ObservableProperty]
+        private ListItemTemplate? _selectedListItem;
+
+        partial void OnSelectedListItemChanged(ListItemTemplate? value)
+        {
+            if (value is null) return;
+            var instance = Activator.CreateInstance(value.ModelType);
+            if (instance == null) return;
+            CurrentPage = (ObservableObject)instance;
+        }
+
+        public ObservableCollection<ListItemTemplate> Items { get; } = new()
+        {
+            new ListItemTemplate(typeof(HomePageViewModel)),
+            new ListItemTemplate(typeof(ButtonPageViewModel)),
+        };
+
+        [RelayCommand]
+        private void TriggerPane()
+        {
+            IsPaneOpen = !IsPaneOpen;
+        }
+    }
+
+    public class ListItemTemplate
+    {
+        public ListItemTemplate(Type type)
+        {
+            ModelType = type;
+            Label = type.Name.Replace("PageViewModel", "");
+        }
+
+        public string Label { get; }
+        public Type ModelType { get; }
+    }
+}
